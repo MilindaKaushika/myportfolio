a28d4bd5267f91efcfa5380fa4110f4a05448f9e
Milinda Kaushika <milindakaushika@outlook.com>
2023-04-21 08:13:55 -0700

    added responsiv

--- js/script.js
+++ js/script.js
@@ -38,3 +38,17 @@ const scriptURL = 'https://script.google.com/macros/s/AKfycbyh_X6-SAzE5NXblht9tx
       })
       .catch(error => console.error('Error!', error.message))
   })
+
+//  Menu list
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () =>{
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+};
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('open');
+};
