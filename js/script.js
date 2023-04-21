eed01941d14b8cc7a85dfba4c37910b8b4e185e5
Milinda Kaushika <milindakaushika@outlook.com>
2023-04-21 03:44:10 -0700

    added contact form

--- js/script.js
+++ js/script.js
@@ -19,3 +19,22 @@ for(tabcontent of tabcontents){
 event.currentTarget.classList.add("active-link");
 document.getElementById(tabname).classList.add("active-tab");
 }
+
+// Contact form
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyh_X6-SAzE5NXblht9txkXBj6eYrQa9kJqkI970WgqSYR35DwFE_Ymw2rqrMZogKPRoA/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const nsg = document.getElementById("msg");
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response =>{
+          msg.innerHTML = "Message sent successfully"
+          setTimeout(function(){
+              
+            msg.innerHTML=""
+          }, 5000)
+          form.reset();
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
